7b871e573b9a39e37c8da957b2750264a4033505
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-11-28 18:40:49 +0000

    chore: Initial setuff to get started
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Jan Takeshi - A journalling tool";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default = mkShell { buildInputs = [ gnumake ]; };
+      });
+}
