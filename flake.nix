eb2d42c62fd809500e4ebfa80b990ac2be1e8038
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-11-30 22:24:33 +0000

    flake: Add package, needs 24.11
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- flake.nix
+++ flake.nix
@@ -1,15 +1,29 @@
 {
-  description = "Jan Takeshi - A journalling tool";
+  description = "Jun Takami - A journalling tool";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        juntakami = pkgs.rustPlatform.buildRustPackage {
+          pname = "juntakami";
+          version = "git";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+          postInstall = ''
+            ln -s $out/bin/juntakami $out/bin/jt
+          '';
+        };
       in with pkgs; {
+        packages = {
+          inherit juntakami;
+          default = juntakami;
+        };
         devShells.default = mkShell { buildInputs = [ gnumake ]; };
       });
 }
