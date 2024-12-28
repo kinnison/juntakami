ee6b67196d2b94043713d7338c7bd12750971f61
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-12-28 11:09:05 +0000

    chore: Remove functions file as unwanted
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- steps/jt.rs
+++
@@ -1,3 +0,0 @@
-use juntakami_steps::subplotlib;
-
-juntakami_steps::jt_binary_on_path!(env!("CARGO_BIN_EXE_juntakami"));
--- tests/jt.rs
+++ tests/jt.rs
@@ -1,1 +1,5 @@
+use juntakami_steps::subplotlib::{self, prelude::*};
+
+juntakami_steps::jt_binary_on_path!(env!("CARGO_BIN_EXE_juntakami"));
+
 subplotlib::codegen!("steps/juntakami.subplot");
